59b112bc6e968c7ad74acc1550054c4123bbfeab
eunjinlee94 <tmmt94@gmail.com>
2022-03-29 15:23:23 +0900

    0329 test file upload

---
+++ eunjin/0329.js
@@ -0,0 +1,23 @@
+console.log("hello world!");
+
+console.log("강한친구 대한육군");
+console.log("강한친구 대한육군");
+
+console.log(`\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|`);
+
+
+console.log(`|\\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"\`    |
+||_/=\\\\__|`);
+
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a+b);
+
