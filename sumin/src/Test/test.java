15ed2f8dc1d8b9d4f25736a661e1dae1eeaa1754
DESKTOP-GGB9FGV\82108 <ssm347574248@gmail.com>
2022-03-29 12:12:44 +0900

    03.29 백준 입출력

---
+++ sumin/src/Test/test.java
@@ -0,0 +1,56 @@
+package Test;
+
+
+import java.util.Scanner;
+
+public class test {
+	public static void main(String[] args) {
+
+		// 1. Hello World
+
+//	System.out.println("Hello World!");
+	
+		// 2. 
+//	System.out.println("강한친구 대한육군");
+//	System.out.println("강한친구 대한육군");
+	
+		// 3. 구현(고양이)
+//		System.out.println("\\    /\\");
+//		System.out.println(" )  ( \')");
+//		System.out.println("(  /  )");
+//		System.out.println(" \\(__)|");
+		
+		// 7. A*B
+//			Scanner sc = new Scanner(System.in);
+//			double num1 = sc.nextDouble();
+//			double num2 = sc.nextDouble();
+//			sc.close();
+//			double num3 = num1 * num2;
+//			System.out.print(num3);
+			
+		//	8. A/B
+//			Scanner sc = new Scanner(System.in);
+//			double num1 = sc.nextDouble();
+//			double num2 = sc.nextDouble();
+//			sc.close();
+//			double num3 = num1 / num2;
+//			System.out.print(num3);
+			
+		// 9. 사칙연산
+			Scanner sc = new Scanner(System.in);
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			int num3 = num1 + num2;
+			int num4 = num1 - num2;
+			int num5 = num1 * num2;
+			int num6 = num1 / num2;
+			int num7 = num1 % num2;
+			sc.close();
+			System.out.println(num3);
+			System.out.println(num4);
+			System.out.println(num5);
+			System.out.println(num6);
+			System.out.println(num7);
+}
+
+}
