54ebc847f2ad89eaa6030e196191a2f8e3aeab3d
yooil-405 <yooil405@gmail.com>
2022-03-29 10:47:24 +0900

    20220329 문제풀이

---
+++ Yooil/src/BAEKJOON/step1/Main.java
@@ -0,0 +1,48 @@
+package BAEKJOON.step1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 1. Hello World
+//		System.out.println("Hello World!");
+//		
+		// 2. We love kriii
+//		System.out.println("강한친구 대한육군");
+//		System.out.println("강한친구 대한육군");
+//		
+//		// 3. 구현(고양이)
+//		System.out.println("\\    /\\");
+//		System.out.println(" )  ( \')");
+//		System.out.println("(  /  )");
+//		System.out.println(" \\(__)|");
+//		
+//		// 7. A×B
+//		Scanner sc = new Scanner(System.in);
+//		int A, B;		
+//		A = sc.nextInt();
+//		B = sc.nextInt();		
+//		System.out.println(A * B);	
+//		
+//		// 8. A/B
+//		Scanner sc = new Scanner(System.in);
+//		double A, B;		
+//		A = sc.nextInt();
+//		B = sc.nextInt();		
+//		System.out.println(A / B);	
+		
+		// 9. 사칙연산		
+		Scanner sc = new Scanner(System.in);
+		int A, B;		
+		A = sc.nextInt();
+		B = sc.nextInt();		
+		System.out.println(A + B);			
+		System.out.println(A - B);		
+		System.out.println(A * B);		
+		System.out.println(A / B);
+		System.out.println(A % B);
+		
+	}
+
+}
