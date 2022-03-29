7239b6e85d88a569fc23899e48cd1739ef0e1647
eunjinlee94 <tmmt94@gmail.com>
2022-03-29 16:20:15 +0900

    220329 덜 풀어서 문제 추가하여 푸시 !

--- eunjin/0329.js
+++ eunjin/0329.js
@@ -1,23 +1,53 @@
+// 1번
 console.log("hello world!");
 
+// 2번
 console.log("강한친구 대한육군");
 console.log("강한친구 대한육군");
 
+// 3번 
 console.log(`\\    /\\
  )  ( ')
 (  /  )
  \\(__)|`);
 
-
+ // 4번 
 console.log(`|\\_/|
 |q p|   /}
 ( 0 )"""\\
 |"^"\`    |
 ||_/=\\\\__|`);
 
+// 5번 
 var fs = require('fs');
 var input = fs.readFileSync('/dev/stdin').toString().split(' ');
 var a = parseInt(input[0]);
 var b = parseInt(input[1]);
 console.log(a+b);
 
+// 7번
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a*b);
+
+// 8번
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a/b);
+
+// 9번
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(Math.floor(a / b));
+console.log(a%b);
+
+// 
