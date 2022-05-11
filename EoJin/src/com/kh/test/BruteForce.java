15aa9e4e5292b42cf8bbbca8435566188baee3f0
jinn13 <dydn512@gmail.com>
2022-05-11 23:54:31 +0900

    220427_문제풀이(진행중)

--- EoJin/src/com/kh/test/BruteForce.java
+++ EoJin/src/com/kh/test/BruteForce.java
@@ -30,59 +30,56 @@ public class BruteForce {
 		}
 		System.out.println(tmp);
 	}
-	
-	
+
 	public void question2() {
 		Scanner sc = new Scanner(System.in);
-        
+
 		int N = sc.nextInt();
-        
+
 		int result = 0;
- 
-		
-		for(int i = 0; i < N; i++) {
+
+		for (int i = 0; i < N; i++) {
 			int number = i;
-			int sum = 0;	// 각 자릿수 합 변수 
-			
-			while(number != 0) {
-				sum += number % 10;	// 각 자릿수 더하기
+			int sum = 0; // 각 자릿수 합 변수
+
+			while (number != 0) {
+				sum += number % 10; // 각 자릿수 더하기
 				number /= 10;
 			}
-			
-			// i 값과 각 자릿수 누적합이 같을 경우 (생성자를 찾았을 경우) 
-			if(sum + i == N) {
+
+			// i 값과 각 자릿수 누적합이 같을 경우 (생성자를 찾았을 경우)
+			if (sum + i == N) {
 				result = i;
 				break;
 			}
-			
+
 		}
- 
+
 		System.out.println(result);
 	}
-	
+
 	public void question3() {
 		Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int size = String.valueOf(n).length(); // n의 자리수를 구하기 위한..
-        int start = n - (9 * size); // 시작 최솟값 구하기
-        int ans = 0;
-        
-        for(int i=start; i<n; i++) {
-            int sum = i; //합
-            int k = i; //한 자리씩 구하기 위한..
-            while(k > 0) {
-                sum += k%10;
-                k /= 10;
-            }
-            if(sum == n) {
-                ans = i;
-                break;
-            }
-        }
-        System.out.println(ans);
+		int n = scan.nextInt();
+		int size = String.valueOf(n).length(); // n의 자리수를 구하기 위한..
+		int start = n - (9 * size); // 시작 최솟값 구하기
+		int ans = 0;
+
+		for (int i = start; i < n; i++) {
+			int sum = i; // 합
+			int k = i; // 한 자리씩 구하기 위한..
+			while (k > 0) {
+				sum += k % 10;
+				k /= 10;
+			}
+			if (sum == n) {
+				ans = i;
+				break;
+			}
+		}
+		System.out.println(ans);
 	}
-	
-	
+
 	// 체스판 칠하기(유보)
 	public void question4() {
 //		Scanner sc = new Scanner(System.in);
@@ -107,5 +104,21 @@ public class BruteForce {
 //				}
 //			}
 //		}
+	}
+
+	public void question5() {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int result = 666;
+		
+		while (true) {
+			if (Integer.toString(result).contains("666"))
+				num--;
+			if (num == 0)
+				break;
+			result++;
+		}
+		System.out.println(result);
 	}
 };
