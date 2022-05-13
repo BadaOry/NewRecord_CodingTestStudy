ef05ade715823a52bf480d55330c8c2dbf80a5b5
jinn13 <dydn512@gmail.com>
2022-05-13 18:09:33 +0900

    220427_문제풀이(진행중)

---
+++ EoJin/src/com/kh/test/Sort.java
@@ -0,0 +1,32 @@
+package com.kh.test;
+
+import java.util.Scanner;
+
+public class Sort {
+	
+	// 수 정렬하기
+	public void question1() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int arr[] = new int[N];
+		int temp;
+		
+		for(int i=0;i<N;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i=0;i<N;i++) {
+			for (int j=i+1;j<N;j++) {
+				if(arr[i]>arr[j]) {
+					temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+		
+		for(int i=0;i<N;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
