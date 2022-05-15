61bcf0f36c0ffc38aee0e0977a1aff8efb1d9af5
jinn13 <dydn512@gmail.com>
2022-05-15 23:57:44 +0900

    220428_문제풀이(진행중)

--- EoJin/src/com/kh/test/Sort.java
+++ EoJin/src/com/kh/test/Sort.java
@@ -1,58 +1,88 @@
 package com.kh.test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sort {
-	
+
 	// 수 정렬하기
 	public void question1() {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int arr[] = new int[N];
 		int temp;
-		
-		for(int i=0;i<N;i++) {
+
+		for (int i = 0; i < N; i++) {
 			arr[i] = sc.nextInt();
 		}
-		
-		for(int i=0;i<N;i++) {
-			for (int j=i+1;j<N;j++) {
-				if(arr[i]>arr[j]) {
-					temp=arr[i];
-					arr[i]=arr[j];
-					arr[j]=temp;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (arr[i] > arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
 				}
 			}
 		}
-		
-		for(int i=0;i<N;i++) {
+
+		for (int i = 0; i < N; i++) {
 			System.out.println(arr[i]);
 		}
 	}
-	
+
 	public void question2() {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int arr[] = new int[N];
 		int temp;
-		
-		for(int i=0;i<N;i++) {
+
+		for (int i = 0; i < N; i++) {
 			arr[i] = sc.nextInt();
 		}
-		
-		for(int i=0;i<N;i++) {
-			for (int j=i+1;j<N;j++) {
-				if(arr[i]>arr[j]) {
-					temp=arr[j];
-					arr[j]=arr[i];
-					arr[i]=temp;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (arr[i] > arr[j]) {
+					temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
 				}
 			}
 		}
-		
-		for(int i=0;i<N;i++) {
+
+		for (int i = 0; i < N; i++) {
 			System.out.println(arr[i]);
 		}
-		
+
+	}
+
+	public void question3() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N;
+		try {
+			N = Integer.parseInt(br.readLine());
+			int[] arr = new int[N];
+
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(br.readLine());
+			}
+			Arrays.sort(arr);
+
+			for (int i = 0; i < N; i++) {
+				sb.append(arr[i]).append('\n');
+			}
+
+			System.out.println(sb);
+		} catch (NumberFormatException | IOException e) {
+
+			e.printStackTrace();
+		}
+
 	}
 }
