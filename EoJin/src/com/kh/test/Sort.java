7218073840487601e8b8459f2ff11796fa6dae3a
jinn13 <dydn512@gmail.com>
2022-05-21 23:23:52 +0900

    220428_문제풀이(진행중)

--- EoJin/src/com/kh/test/Sort.java
+++ EoJin/src/com/kh/test/Sort.java
@@ -85,4 +85,51 @@ public class Sort {
 		}
 
 	}
+
+	
+	// 통계학
+	public void question4() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		double[] num = new double[N];
+
+		for (int i = 0; i < N; i++)
+			num[i] = sc.nextDouble();
+		sc.close(); // 산술평균
+
+		double sum = 0;
+
+		for (double d : num)
+			sum += d;
+		System.out.println((int) Math.round(sum / N)); // 중앙값
+
+		Arrays.sort(num);
+		System.out.println((int) num[N / 2]); // 최빈값
+
+		int[] freq = new int[8001]; 
+
+		for (double d : num)
+			freq[(int) d + 4000]++; 
+
+		int maxFrequency = 0; // 최대 빈도
+		int maxIndex = 0; 
+		for (int i = 0; i < 8001; i++) 
+			if (freq[i] > maxFrequency)
+				maxFrequency = freq[i];
+
+		boolean twice = false; 
+		for (int i = 0; i < 8001; i++) {
+			if (freq[i] == maxFrequency) { 
+				if (twice) {
+					maxIndex = i - 4000;
+					break;
+				}
+				maxIndex = i - 4000;
+				twice = true; 
+			}
+		}
+		System.out.println(N == 1 ? (int) num[0] : maxIndex); // 범위
+		System.out.println((int) (num[N - 1] - num[0]));
+	}
+
 }
