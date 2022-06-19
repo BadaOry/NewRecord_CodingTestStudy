26b05f1ad8c22c40cf6dcfd4d47f28fbb63f342b
jinn13 <dydn512@gmail.com>
2022-06-19 23:47:20 +0900

    20220619

--- EoJin/src/com/kh/test/Main.java
+++ EoJin/src/com/kh/test/Main.java
@@ -1,99 +1,35 @@
 package com.kh.test;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-
-		// 1번 문제
-//		System.out.println("Hello World!");
-//		
-//		// 2번 문제
-//		System.out.println("강한친구 대한육군");
-//		System.out.println("강한친구 대한육군");
-//		
-//		// 3번 문제
-//        System.out.println("\\    /\\");
-//        System.out.println(" )  ( ')");
-//        System.out.println("(  /  )");
-//        System.out.println(" \\(__)|");
-//
-//		// 4번 문제
-//        System.out.println("|\\_/|");
-//        System.out.println("|q p|   /}");
-//        System.out.println("( 0 )\"\"\"\\");
-//        System.out.println("|\"^\"`    |");
-//        System.out.println("||_/=\\\\__|");
-        
-		// 5번 문제
-//		Scanner sc = new Scanner(System.in);
-//		int A = sc.nextInt();
-//		int B= sc.nextInt();
-//		System.out.println(A+B);
-			
-		// 6번 문제
-//		Scanner sc = new Scanner(System.in);
-//		int A = sc.nextInt();
-//		int B = sc.nextInt();
-//		System.out.println(A-B);
+//		ConditionalStatement cs = new ConditionalStatement();
+//		RepetitiveStatement rs = new RepetitiveStatement();
+//		OneDimensionalArray da = new OneDimensionalArray();
+//		Function ft = new Function();
+//		characterString cs = new characterString();
+//		ArithmeticFirst af = new ArithmeticFirst();
+//		ArithmeticSecond as = new ArithmeticSecond();
+//		BruteForce bf = new BruteForce();
+		Sort st = new Sort();
 		
-		// 7번 문제
-//		Scanner sc = new Scanner(System.in);
-//		int A = sc.nextInt();
-//		int B= sc.nextInt();
-//		System.out.println( A * B );
+//		int a[] = {1, 4, 7, 8};	
+//		System.out.println(ft.sum(a));		
 		
-		// 8번 문제
-//		Scanner sc = new Scanner(System.in);
-//		Double A = sc.nextDouble();
-//		Double B= sc.nextDouble();
-//		System.out.println( A / B );
+//		st.question1();
+//		st.question2();
+//		st.question3();
+//		st.question4();
+//		st.question5();
+//		st.question6();
+		st.question7();
+//		st.question8();
+//		as.question9();
+//		as.question10();
+//		as.question11();
+//		rs.question12();
+//		rs.question13();
+//		rs.question14();
 		
-		// 9번 문제
-//		Scanner sc = new Scanner(System.in);
-//		int A = sc.nextInt();
-//		int B= sc.nextInt();
-//		System.out.println( A + B );
-//		System.out.println( A - B );
-//		System.out.println( A * B );
-//		System.out.println( A / B );
-//		System.out.println( A % B );
-		
-		// 10번 문제
-//		Scanner sc = new Scanner(System.in);
-//		String A = sc.next();
-//		System.out.println( A + "??!");
-		
-		// 11번 문제
-//		Scanner sc = new Scanner(System.in);
-//		int A = sc.nextInt();
-//		System.out.println( A - 543);
-		
-		// 12번 문제
-//		Scanner sc = new Scanner(System.in);
-//		int A = sc.nextInt();
-//		int B = sc.nextInt();
-//		int C = sc.nextInt();
-//		System.out.println((A + B) % C);
-//		System.out.println(((A % C) + (B % C))%C);
-//		System.out.println((A * B) % C);
-//		System.out.println(((A % C) * (B % C))%C);
-		
-		
-		// 13번 문제
-		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-        String C = String.valueOf(B);
-        String[] digits = C.split("(?<=.)");
-        
-		System.out.println(A * Integer.parseInt(digits[2]));
-		System.out.println(A * Integer.parseInt(digits[1]));
-		System.out.println(A * Integer.parseInt(digits[0]));
-		
-		System.out.println(A * B);
-
 	}
-
 }
