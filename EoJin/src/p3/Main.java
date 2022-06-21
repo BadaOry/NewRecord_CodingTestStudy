aa69495bbcb5ccd53d4ce9b33fe2895ab31a15be
jinn13 <dydn512@gmail.com>
2022-06-21 23:37:55 +0900

    20220621

---
+++ EoJin/src/p3/Main.java
@@ -0,0 +1,13 @@
+package p3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+
+		int result = solution.solution(9);
+		
+		System.out.println("결과 : "+result);
+	}
+
+}
