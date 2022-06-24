bf2114849cb3e61fabb1750e2a4535cc3378c52f
jinn13 <dydn512@gmail.com>
2022-06-24 23:45:44 +0900

    20220624

--- EoJin/src/com/kh/test/Sort.java
+++ EoJin/src/com/kh/test/Sort.java
@@ -183,7 +183,6 @@ public class Sort {
 			arr[i][1] = sc.nextInt();
 			arr[i][0] = sc.nextInt();
 		}
-		
 
 		Arrays.sort(arr, (e1, e2) -> {
 			if(e1[0] == e2[0]) {
@@ -223,10 +222,39 @@ public class Sort {
 				System.out.println(arr[i]);
 				temp = arr[i];
 			}
-		}
-
-
-		
+		}	
 	}
 
+	public void question9() {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		String[][] arr = new String[N][2];
+		
+ 
+		for(int i = 0; i < N; i++) {
+			arr[i][0] = sc.next();	// 나이
+			arr[i][1] = sc.next();	// 이름
+		}
+ 
+		
+		Arrays.sort(arr, new Comparator<String[]>() {
+			// 나이순으로 정렬
+			@Override
+			public int compare(String[] s1, String[] s2) {
+				return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+			}
+			
+		});
+ 
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < N; i++) {
+			sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+		}
+        
+		System.out.println(sb);
+		
+	}
+		
 }
